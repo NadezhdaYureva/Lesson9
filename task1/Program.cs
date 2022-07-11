fae71debc4bfdd154c9a23a9934da39d5fa40a45
nadezhdayu <nadezhdayureva1987@gmail.com>
2022-07-11 16:27:08 +0300

    Решение задачи 1

---
+++ task1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+void RecursiveMToN(int M, int N)
+{
+    if (N < M)
+    {
+        return;
+    }
+    else
+    {
+        {
+            if (M % 2 == 0)
+            {
+                System.Console.Write($"{M}  ");
+            }
+            RecursiveMToN(M + 1, N);
+        }
+    }
+}
+
+int Prompt(string console)
+{
+    System.Console.WriteLine(console);
+    string numberStr = Console.ReadLine();
+    return int.Parse(numberStr);
+}
+
+void Solve()
+{
+    int m = Prompt("Введите число M");
+    int n = Prompt("Введите число N");
+    RecursiveMToN(m, n);
+
+}
+
+Solve();
